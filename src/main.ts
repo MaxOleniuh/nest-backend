31ee8c76e55afc070c772ca3187f33eedcc78305
Max <maxim.oleniukh@gmail.com>
2025-11-07 10:52:29 +0000

    global pipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "./pipes/validation.pipe";
 
 async function start() {
   const PORT = process.env.PORT || 5000;
@@ -8,6 +9,8 @@ async function start() {
   await app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
+
+  app.useGlobalPipes(new ValidationPipe());
 }
 
 start();
